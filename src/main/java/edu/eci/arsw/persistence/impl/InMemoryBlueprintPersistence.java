a71f61323d408124b2212820681d4f8e3355a0da
andres.sepulveda-s <jacobosepul12@gmail.com>
2025-09-06 09:32:30 -0500

    Punto 2

--- src/main/java/edu/eci/arsw/persistence/impl/InMemoryBlueprintPersistence.java
+++ src/main/java/edu/eci/arsw/persistence/impl/InMemoryBlueprintPersistence.java
@@ -28,9 +28,19 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
 
     public InMemoryBlueprintPersistence() {
         //load stub data
-        Point[] pts=new Point[]{new Point(140, 140),new Point(115, 115)};
-        Blueprint bp=new Blueprint("_authorname_", "_bpname_ ",pts);
-        blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
+        Point[] pts1 = new Point[]{new Point(10, 10), new Point(20, 20)};
+        Blueprint bp1 = new Blueprint("john", "house", pts1);
+        blueprints.put(new Tuple<>(bp1.getAuthor(), bp1.getName()), bp1);
+
+        // Plano 2 - Autor: john (mismo autor que bp1)
+        Point[] pts2 = new Point[]{new Point(30, 30), new Point(40, 40)};
+        Blueprint bp2 = new Blueprint("john", "car", pts2);
+        blueprints.put(new Tuple<>(bp2.getAuthor(), bp2.getName()), bp2);
+
+        // Plano 3 - Autor: alice (autor diferente)
+        Point[] pts3 = new Point[]{new Point(50, 50), new Point(60, 60)};
+        Blueprint bp3 = new Blueprint("alice", "tree", pts3);
+        blueprints.put(new Tuple<>(bp3.getAuthor(), bp3.getName()), bp3);
         
     }    
     
